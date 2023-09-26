07c485af90c6c4ff326f4200fe5dd4ead6e4f37e
RyoHatano <ryohatano0809@gmail.com>
2023-09-26 17:05:39 +0900

    シリアライザにバリデーションメソッドを追加した例

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,6 @@
+from django.core.validators import RegexValidator
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Book
 
@@ -7,7 +9,37 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         exclude = ['created_at']
+        validator = [
+            # タイトルと価格でユニークになっていることを検証
+            UniqueTogetherValidator(
+                queryset=Book.objects.all(),
+                fields=('title', 'price'),
+                message="タイトルと価格でユニークになっていなければいけません。"
+            ),
+        ],
+        extra_kwargs = {
+            'title': {
+                'validators': [
+                    RegexValidator(
+                        r'^D.+$', message="タイトルはDで始まる必要があります。"
+                    ),
+                ],
+            },
+        }
         
+        
+def validate_title(self, value):
+    if "Java" in value:
+        raise serializers.ValidationError("Javaは含めません。")
+    return value
+
+def validate(self, data):
+    title = data.get('title')
+    price = data.get('price')
+    if title and '薄い本' in title and price and price > 3000:
+        raise serializers.ValidationError("薄い本は3000円を超えてはいけません。")
+    return data
+
 
 class BookListSerializer(serializers.ListSerializer):
     """複数の本モデルを扱うためのシリアライザ"""
