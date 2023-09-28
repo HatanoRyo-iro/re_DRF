ae9917329d8a88527c76e735041ed9d1a4ef4097
RyoHatano <ryohatano0809@gmail.com>
2023-09-28 17:24:45 +0900

    APIView を継承した取得（一覧） API クラスの例

--- api/views.py
+++ api/views.py
@@ -1,44 +1,62 @@
+from django_filters import rest_framework as filters
 from rest_framework import status, views
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookListCreateAPIView(views.APIView):
-    def get(self, request, *args, **kwargs):
-        book_list = Book.objects.all()
-        serializer = BookSerializer(instance=book_list, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request, *args, **kwargs):
-        serializer = BookSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class BookFilter(filters.FilterSet):
+    class Meta:
+        model = Book
+        fields = '__all__'
 
-class BookRetrieveUpdateDestroyAPIView(views.APIView):
-    def get(self, request, pk, *args, **kwargs):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(instance=book)
-        return Response(serializer.data)
+
+class BookListAPIView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        
+        # モデルオブジェクトをクエリ文字を使ってフィルタリングした結果を取得
+        filterset = BookFilter(request.query_params, queryset=Book.objects.all())
+        if not filterset.is_valid():
+            raise ValidationError(filterset.errors)
+        serializer = BookSerializer(instance=filterset.qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# class BookListCreateAPIView(views.APIView):
+#     def get(self, request, *args, **kwargs):
+#         book_list = Book.objects.all()
+#         serializer = BookSerializer(instance=book_list, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def put(self, request, pk, *args, **kwargs):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(instance=book, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+#     def post(self, request, *args, **kwargs):
+#         serializer = BookSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# class BookRetrieveUpdateDestroyAPIView(views.APIView):
+#     def get(self, request, pk, *args, **kwargs):
+#         book = get_object_or_404(Book, pk=pk)
+#         serializer = BookSerializer(instance=book)
+#         return Response(serializer.data)
     
-    def patch(self, request, pk, *args, **kwargs):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(instance=book, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+#     def put(self, request, pk, *args, **kwargs):
+#         book = get_object_or_404(Book, pk=pk)
+#         serializer = BookSerializer(instance=book, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
     
-    def delete(self, request, pk, *args, **kwargs):
-        book = get_object_or_404(Book, pk=pk)
-        book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def patch(self, request, pk, *args, **kwargs):
+#         book = get_object_or_404(Book, pk=pk)
+#         serializer = BookSerializer(instance=book, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+#     def delete(self, request, pk, *args, **kwargs):
+#         book = get_object_or_404(Book, pk=pk)
+#         book.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
