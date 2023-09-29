1d5b70a95ad8e8a7f50a44a9cc51327e5dd9c68e
RyoHatano <ryohatano0809@gmail.com>
2023-09-30 00:50:29 +0900

    APIView を継承した更新・一部更新API クラスの例.

--- api/views.py
+++ api/views.py
@@ -14,11 +14,20 @@ class BookFilter(filters.FilterSet):
         fields = '__all__'
 
 
-class BookRetriveAPIView(views.APIView):
-    def get(self, request, pk, *args, **kwargs):
+class BookUpdateAPIView(views.APIView):
+    def put(self, request, pk, *args, **kwargs):
         book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(instance=book)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = BookSerializer(instance=book, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def patch(self, request, pk, *args, **kwargs):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(instance=book, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 # class BookListCreateAPIView(views.APIView):
 #     def get(self, request, *args, **kwargs):
